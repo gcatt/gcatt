937447cde74c0a1a7eb6ac925d690bba29db94d4
Marius Gisler <marius.gisler@hotmail.com>
2020-03-27 19:09:29 +0100

    Insert Crawler

--- 9gag_Crawler.py
+++ 9gag_Crawler.py
@@ -1,2 +1,13 @@
 ### insert Crawler
 
+from bs4 import BeautifulSoup
+import pandas as pd
+
+data = []
+time.sleep(1)
+url = "https://9gag.com/coronavirus"
+r = requests.get(url, verify = False)
+doc = BeautifulSoup(r.text, 'html.parser')
+
+
+
