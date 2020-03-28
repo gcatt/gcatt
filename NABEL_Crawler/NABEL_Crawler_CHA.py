ba4d2eae6bc11fab0262839a8b5a5fde1028ab00
Gino Cattelan <gino.cattelan@stud.hslu.ch>
2020-03-28 11:52:31 +0100

    All Nabel crawlers

---
+++ NABEL_Crawler/NABEL_Crawler_CHA.py
@@ -0,0 +1,59 @@
+from selenium import webdriver
+import time
+from bs4 import BeautifulSoup
+import pandas as pd
+
+import xlrd
+
+from selenium.webdriver.chrome.options import Options
+chrome_options = Options()
+chrome_options.add_argument("--incognito")
+chrome_options.add_argument("--window-size=1920x1080")
+
+driver = webdriver.Chrome(executable_path = r'C:\Users\ginoc\Downloads\chromedriver_win32\chromedriver.exe')
+
+driver.get('https://www.bafu.admin.ch/bafu/de/home/themen/luft/zustand/daten/datenabfrage-nabel.html')
+
+time.sleep(2)
+
+select_nach_stationen_field = driver.find_element_by_partial_link_text('Abfrage nach Station')
+select_nach_stationen_field.click()
+time.sleep(1)
+
+# Choose Station
+select_station = driver.find_element_by_xpath('//*[@id="station_select_chosen"]')
+select_station.send_keys('Chaum')    ### Change per Station ###
+time.sleep(1)
+
+select_select_all = driver.find_element_by_xpath('//*[@id="station"]/p[1]/button[1]')
+select_select_all.click()
+time.sleep(1)
+
+select_data_type = driver.find_element_by_xpath('//*[@id="tagesmittelwerte"]')
+select_data_type.click()
+time.sleep(1)
+
+select_timespan_type = driver.find_element_by_id('frei')
+select_timespan_type.click()
+time.sleep(1)
+
+select_timespan_from = driver.find_element_by_id('von')
+select_timespan_from.clear()
+select_timespan_from.send_keys('2000-01-01')
+time.sleep(1)
+
+select_timespan_to = driver.find_element_by_xpath('//*[@id="abfrage_form"]/fieldset[2]/div[2]/div[2]/div/button')
+select_timespan_to.click()
+time.sleep(1)
+
+select_csv = driver.find_element_by_id('csv')
+select_csv.click()
+time.sleep(1)
+
+csv_download = driver.find_element_by_xpath('//*[@id="abfrage_form"]/p/button[1]')
+time.sleep(1)
+csv_download.click()
+time.sleep(15)
+
+print('*************************!!!DOWNLOAD COMPLETED!!!*************************')
+driver.quit()
