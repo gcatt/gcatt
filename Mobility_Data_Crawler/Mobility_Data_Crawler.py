0d9677ff6502187148132deb361489c6bc81ddb5
Marius Gisler <marius.gisler@hotmail.com>
2020-03-28 10:46:48 +0100

    NZZ Headlines Corona

--- Mobility_Data_Crawler/Mobility_Data_Crawler.py
+++ Mobility_Data_Crawler/Mobility_Data_Crawler.py
@@ -1,10 +1,23 @@
 import pandas as pd
+import matplotlib.pyplot as plt
 
 pd.DataFrame()
 
 df1 = pd.read_csv("Distanzkategorien_in_Prozent_pro_Tag.csv", sep = ';', header=0)
 
-print(df1)
+
+
+
+is_distanz = df1["Beschreibung"]=="Distanz"
+
+df_distanz = df1[is_distanz]
+
+
+plt.plot(df_distanz)
+
+
+
+
 
 
 
